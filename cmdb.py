c16a79301bb33945fd4a29dd252197c8de2f73dd
David Roddick <droddic2@une.edu.au>
2022-10-18 16:48:52 +1100

    updates

--- cmdb.py
+++ cmdb.py
@@ -3,7 +3,7 @@
 import os
 import sys
 import pprint
-
+import argparse
 from snow import Snow
 
 sn_instance = 'unedev'
@@ -14,21 +14,50 @@ path = '/table/' + ci_table
 
 
 def main():
+  parser = argparse.ArgumentParser()
+
+  parser.add_argument('--debug')
+  parser.add_argument('--debug-all', action='store_true')
+  parser.add_argument('--add', action='store_true')
+  parser.add_argument('--update', action='store_true')
+  parser.add_argument('--delete')
+  parser.add_argument('--delete-all', action='store_true')
+  args = parser.parse_args()
+
   pp = pprint.PrettyPrinter(indent=4)
   now = Snow(sn_instance, sn_user, sn_pw, path)
-  #pp.pprint(hosts)
-   
-  #hosts = now.get_all_hosts()
-  #for host in hosts:
-  #  for key in host.keys():
-  #    now.delete_host(key)
 
-  folder = 'out'
-  for filename in os.listdir(folder):
-    f = os.path.join(folder, filename)
-    if os.path.isfile(f):
-      host = now.generate_ci_info(f)
-      now.add_host(host)
+  if args.debug:
+    pp.pprint(now.get_host(args.debug))
+  elif args.debug_all:
+    pp.pprint(now.get_all_hosts()) 
+  elif args.add: 
+    folder = 'out'
+    for filename in os.listdir(folder):
+      f = os.path.join(folder, filename)
+      if os.path.isfile(f):
+        host = now.generate_ci_info(f)
+        if host is "unreachable":
+          print("{} is unreachable.".format(filename))
+        else:
+          now.add_host(host)
+  elif args.update: 
+    folder = 'out'
+    for filename in os.listdir(folder):
+      f = os.path.join(folder, filename)
+      if os.path.isfile(f):
+        host = now.generate_ci_info(f)
+        if host is "unreachable":
+          print("{} is unreachable.".format(filename))
+        else:
+          now.update_host(host)
+  elif args.delete:
+    now.delete_host(args.delete)
+  elif args.delete_all:
+    hosts = now.get_all_hosts()
+    for host in hosts:
+      for key in host.keys():
+        now.delete_host(key)
 
 if __name__ == '__main__':
   main()
--- snow.py
+++ snow.py
@@ -35,25 +35,34 @@ class Snow():
       return None
 
   def update_host(self, data):
-    sys_id = self.get_sysid(data['name'])
-    if sys_id is None:
-      print("Host doesn't exist")
-      exit()
+    try:
+      sys_id = self.get_sysid(data['name'])
+      if sys_id is None:
+        print("Host doesn't exist")
+        exit()
 
-    resp = self.cmdb.update(query={'sys_id': sys_id}, payload=data)
-    if resp._response.status_code == 200:
-      print(sys_id + ": Updated Successfully.")
-    else:
-      print("Probably not a success")
+      resp = self.cmdb.update(query={'sys_id': sys_id}, payload=data)
+      if resp._response.status_code == 200:
+        print(sys_id + ": Updated Successfully.")
+      else:
+        print("Probably not a success")
+    except KeyError as err:
+      print("Key Error: " + err)
+    except TypeError as err:
+      print("Type Error:" + err)
 
   def add_host(self, data):
-    host = self.get_host(data['name'])
-    if host is not None:
-      print("Host already exists. Maybe you wanted to update?")
-      exit()
-
-    result = self.cmdb.create(payload=data)
-    print("Host added. Nothing bad happened.")
+    try:
+      host = self.get_host(data['name'])
+      if host is not None:
+        print("{} already exists.".format(data['name']))
+      else:
+        result = self.cmdb.create(payload=data)
+        print("{} added successfully.".format(data['name']))
+    except KeyError as err:
+      print(err)
+    except TypeError as err:
+      print(err)
 
   def delete_host(self, host):
     sys_id = self.get_sysid(host)
@@ -62,8 +71,8 @@ class Snow():
       exit()
 
     print("Deleting host: " + host + ", sys_id: " + sys_id)
-    #ch = input("Are you sure? (y/n) ")
-    ch = 'y'
+    ch = input("Are you sure? (y/n) ")
+    #ch = 'y'
     if ch == 'n' or ch == 'N':
       exit()
     elif ch == 'y' or ch == 'Y':
@@ -75,8 +84,12 @@ class Snow():
       exit()
 
   def parse_group(self, groups):
-    remove = ['OracleLinux-6','OracleLinux-7','OracleLinux-8','VMware-guest','dc_une',
-          'dc_une-resources_private','resources_private','dc_eq','dc_eq-resources_private']
+    remove = ['OracleLinux-6','OracleLinux-7','OracleLinux-8',
+              'VMware-guest','dc_une','dc_une-resources_private',
+              'resources_private','dc_eq','dc_eq-resources_private', 
+              'dc_une-resources_public','resources_public','kvm-host',
+              'dc_gs','dc_gs-resources_private','CentOS-6','CentOS-7',
+              'dc_udc','dc_udc-resources_private','eq','eq_dev','eq_prod']
     for rm in remove[:]:
       if rm in groups:
         groups.remove(rm)
@@ -86,18 +99,23 @@ class Snow():
     ci = {}
     f = open(datafile)
     data = json.load(f)
-    ci['name'] = data['ansible_facts']['ansible_hostname']
-    ci['host_name'] = data['ansible_facts']['ansible_hostname']
-    ci['fqdn'] = data['ansible_facts']['ansible_fqdn']
-    ci['ip_address'] = data['ansible_facts']['ansible_default_ipv4']['address']
-    ci['short_description'] = data['ansible_facts']['ansible_local']['system_info']['system_information']['system_info_note']
-    if data['ansible_facts']['ansible_os_family'] == 'RedHat':
-      ci['os'] = 'Linux Red Hat'
+    if 'unreachable' in data:
+      return "unreachable"
     else:
-      ci['os'] = 'GNU/Linux'
-    ci['os_version'] = data['ansible_facts']['ansible_distribution_version']
-    g = self.parse_group(data['ansible_facts']['ansible_local']['system_info']['server']['ansible_groups'])
-    ci['asset_tag'] = ','.join(g)
-    
-    f.close()
+      try:
+        ci['name'] = data['ansible_facts']['ansible_hostname']
+        ci['host_name'] = data['ansible_facts']['ansible_hostname']
+        ci['fqdn'] = data['ansible_facts']['ansible_fqdn']
+        ci['ip_address'] = data['ansible_facts']['ansible_default_ipv4']['address']
+        ci['short_description'] = data['ansible_facts']['ansible_local']['system_info']['system_information']['system_info_note']
+        if data['ansible_facts']['ansible_os_family'] == 'RedHat':
+          ci['os'] = 'Linux Red Hat'
+        else:
+          ci['os'] = 'GNU/Linux'
+        ci['os_version'] = data['ansible_facts']['ansible_distribution_version']
+        g = self.parse_group(data['ansible_facts']['ansible_local']['system_info']['server']['ansible_groups'])
+        ci['attributes'] = json.dumps(g)
+      except KeyError as err:
+        print("Key Error:" + err)
+      f.close()
     return ci
